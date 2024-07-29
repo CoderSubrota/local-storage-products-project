a923b76038ba66fb1d570e95c2feec3957f87d96
subrota <itsectorcommunication@gmail.com>
2024-07-29 00:36:13 -0700

    add update new codes

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let productName = document.querySelector("#productName").value ;
 let productPrice = document.querySelector("#productPrice").value ;
 let brandName = document.querySelector("#brandName").value ;
 let productImage = document.querySelector("#productImage").value ;
+if(productImage==="" || productName==="" || brandName==="" || productPrice==="") return alert("Please fill out the form !!");
 //push data into the array
 let products = JSON.parse(localStorage.getItem("products") || "[]");
 //create an object for get the product data
